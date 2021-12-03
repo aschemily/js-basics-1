d7d3a699c2c8d43bea8e7977ec1caade7a0a2897
aschemily <aschemily@gmail.com>
2021-12-03 09:55:30 -0500

    gitinfo done

--- gitInfo.js
+++ gitInfo.js
@@ -11,6 +11,8 @@
 
 //CODE HERE
 
+let gitDefinition = "Gir is a version control system that helps with project management. from big to small projects. You can also work with team members to manage workflow, changes to project, etc."
+
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,6 +20,8 @@
 */
 
 //CODE HERE
+
+let gitHubDefinition = "Github is the platform that allows you to access your projects via cloud. Once projects uploaded into the cloud you can download into your system locally, make changes that wouldn't affect existing project unless you tried to re-upload(push)."
 
 //////////////////PROBLEMS 3 - 8////////////////////
 /*
@@ -35,6 +39,15 @@
 
 //CODE HERE
 
+const init ={
+    description: "creates a new git repo",
+    gitInit: "git init",
+    gitAdd: "git add .",
+    gitCommit: `git commit -m"comment"`,
+    gitPush: `git push -u origin main`
+
+}
+
 //////////////////PROBLEM 4////////////////////
 /*
     Create an object called 'clone' with 'description' and 'code' properties 
@@ -43,6 +56,12 @@
 
 //CODE HERE
 
+const clone = {
+    description:"makes a copy of the repo and saves it locally to your device",
+    copyRepo: "in github you need to go to repo andcopy HTTPS url",
+    inTerminal: "git clone (url) -> should say done once completed"
+}
+
 //////////////////PROBLEM 5////////////////////
 /*
     Create an object called 'status' with 'description' and 'code' properties 
@@ -50,6 +69,10 @@
 */
 
 //CODE HERE
+const status = {
+    description:"tells you the state of the working directory and what changes have been staged. ie; git files have been modified.",
+    inTerminal: "git status"
+}
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -59,6 +82,11 @@
 
 //CODE HERE
 
+const add = {
+    description:"adds the changes you made in the working directory.",
+    inTerminal: "git add ."
+}
+
 //////////////////PROBLEM 7////////////////////
 /*
     Create an object called 'commit' with 'description' and 'code' properties
@@ -66,6 +94,10 @@
 */
 
 //CODE HERE
+const commit = {
+    description:"takes a snapshot of the changes you made after you've added them.",
+    inTerminal: "git commit -m(write a description no parenthesis though)"
+}
 
 
 //////////////////PROBLEM 8////////////////////
@@ -75,3 +107,8 @@
 */
 
 //CODE HERE
+
+const push = {
+    description:"uploads all the changes you made to the repo, which can then be accessed/seen by others and cloned on other devices",
+    inTerminal:"when you first push you will have to use git push -u origin main but after you've done that you could just use git push for the remainder of your work"
+}
