c9a2cd979ab2565a8f18a6bcc5095acb59169115
aschemily <aschemily@gmail.com>
2021-12-03 09:25:47 -0500

    software development lifecycle

--- sldc.js
+++ sldc.js
@@ -12,8 +12,22 @@
 // Planning
 // Testing & Integration
 
+//planning
+//analysis of requirements
+//design
+//Implementation
+//Testing & Integration
+//Maintain
+
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
+
+let analysisOfRequirements = "What's the mvp and features it needs. What are the necessary tools, resources to build it out."
+let design = "How will this product look? UI/UX, architecture, security, etc."
+let implementation = "Coding process, translating requirements, design into code. Also includes documentation"
+let maintain = "What needs to be done while in production? checking bug fixes, release of new features or third party dependents."
+let planning = "Working with stakeholders, engineers, design, etc. to come up with an efficient plan for the product. What does it need, how much will it cost (labor, materials, etc.)"
+let testingAndIntegration = "Once implemented, testing with QA, beta-users, etc. Find bugs, security, design flaws."
